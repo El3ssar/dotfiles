4ab8f47aca540b0919eeac5b7e2ee3afc6c9fff6
Daniel Estevez <kemossabee@gmail.com>
2025-10-19 20:47:43 +0200

    Full fizsh

---
+++ dot_fizsh/dot_zsh_plugins.zsh
@@ -0,0 +1,50 @@
+fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-getantidote-SLASH-use-omz" )
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-getantidote-SLASH-use-omz/antidote-use-omz.plugin.zsh"
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-getantidote-SLASH-use-omz/use-omz.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-ohmyzsh/plugins/git" )
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-ohmyzsh/plugins/git/git.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-ohmyzsh/plugins/cp" )
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-ohmyzsh/plugins/cp/cp.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-ohmyzsh/plugins/fzf" )
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-ohmyzsh/plugins/fzf/fzf.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-ohmyzsh/plugins/emoji" )
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-ohmyzsh/plugins/emoji/emoji.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-ohmyzsh/plugins/pip" )
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-ohmyzsh/plugins/pip/pip.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-ohmyzsh/plugins/python" )
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-ohmyzsh/plugins/python/python.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-ohmyzsh/plugins/rust" )
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-ohmyzsh/plugins/rust/rust.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-ohmyzsh/plugins/screen" )
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-ohmyzsh/plugins/screen/screen.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-ohmyzsh/plugins/sudo" )
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-ohmyzsh/plugins/sudo/sudo.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-ohmyzsh/plugins/vscode" )
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-ohmyzsh/plugins/vscode/vscode.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-ohmyzsh/plugins/zoxide" )
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-ohmyzsh/plugins/zoxide/zoxide.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-ohmyzsh/plugins/copypath" )
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-ohmyzsh/plugins/copypath/copypath.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-ohmyzsh/plugins/poetry" )
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-ohmyzsh/plugins/poetry/poetry.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-rutchkiwi-SLASH-copyzshell" )
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-rutchkiwi-SLASH-copyzshell/copyzshell.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-hlissner-SLASH-zsh-autopair" )
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-hlissner-SLASH-zsh-autopair/autopair.plugin.zsh"
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-hlissner-SLASH-zsh-autopair/zsh-autopair.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting" )
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-zsh-yazi-cwd" )
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-zsh-yazi-cwd/yazi-cwd.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-zsh-eza" )
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-zsh-eza/zsh-eza.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-zsh-bat" )
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-zsh-bat/zsh-bat.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-zsh-autosuggestions" )
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-z-shell-SLASH-zsh-diff-so-fancy" )
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-z-shell-SLASH-zsh-diff-so-fancy/zsh-diff-so-fancy.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-zsh-uv-env" )
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-zsh-uv-env/zsh-uv-env.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-zsh-clover-prompt" )
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-El3ssar-SLASH-zsh-clover-prompt/clover.plugin.zsh"
